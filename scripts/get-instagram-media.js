e7dc4b5760c9c8505168fee90cfc3514533dac51
Paulo Diovani Gonçalves <1295197+paulodiovani@users.noreply.github.com>
2024-05-14 15:11:41 -0300

    Fetch by username
    - add yargs dependency

--- scripts/get-instagram-media.js
+++ scripts/get-instagram-media.js
@@ -1,17 +1,26 @@
 const axios = require('axios');
+const yargs = require('yargs/yargs');
 
 const ITEM_COUNT = 50; // max == 50
 
 // main function
 async function main() {
-  const userId = 66194687083;
+  const argv = yargs(process.argv.slice(2))
+    .option('username', {
+      alias: 'u',
+      description: 'Instagram account username',
+    })
+    .demandOption(['username'])
+    .help()
+    .parse();
 
   try {
-    const mediaItems = await fetchInstagramMedia(userId);
-    // console.log(response.data);
+    const userInfo = await fetchInstagramUserInfo(argv.username);
+
+    const userId = userInfo.response.body.data.user.id;
+    const mediaItems = await fetchInstagramMedia(parseInt(userId));
     console.log(JSON.stringify(mediaItems, null, 2));
   } catch (error) {
-    // console.error(error);
     console.error(JSON.stringify(error, null, 2));
   }
 }
@@ -26,7 +35,6 @@ async function rapidApiRequest(path, data = {}) {
   const rapidApiKey = env('RAPID_API_INSTAGRAM_KEY');
   const rapidApiHost = env('RAPID_API_INSTAGRAM_HOST');
 
-  debugger
   const options = {
     method: 'POST',
     url: `https://${rapidApiHost}${path}`,
@@ -42,13 +50,19 @@ async function rapidApiRequest(path, data = {}) {
   return response.data;
 }
 
+async function fetchInstagramUserInfo(username) {
+  const data = { username };
+
+  return rapidApiRequest('/instagram/user/get_info', data);
+}
+
 async function fetchInstagramMedia(userId, maxId = null) {
   const data = {
     id: userId,
     count: ITEM_COUNT,
     max_ix: maxId,
   };
-  
+
   return rapidApiRequest('/instagram/user/get_media', data)
 }
 
