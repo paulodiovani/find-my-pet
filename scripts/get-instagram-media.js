1650cdeb2693ee006cae0577686d45b2526d4a6e
Paulo Diovani Gonçalves <1295197+paulodiovani@users.noreply.github.com>
2024-05-14 16:19:09 -0300

    Create a CSV file

--- scripts/get-instagram-media.js
+++ scripts/get-instagram-media.js
@@ -1,6 +1,7 @@
 const R = require('ramda');
 const axios = require('axios');
 const yargs = require('yargs/yargs');
+const { writeToString: csv } = require('fast-csv');
 
 const ITEM_COUNT = 50; // max == 50
 
@@ -22,6 +23,8 @@ function main() {
 
 const toJson = R.curry(JSON.stringify)(R.__, null, 2);
 
+const toCsv = R.curry(csv)(R.__, { headers: true });
+
 const pipeline = R.pipe(
   fetchInstagramUserInfo,
   R.andThen(R.path(['response', 'body', 'data', 'user', 'id'])),
@@ -29,7 +32,8 @@ const pipeline = R.pipe(
   R.andThen(fetchInstagramMedia),
   R.andThen(R.path(['response', 'body', 'items'])),
   R.andThen(R.map(formatMedia)),
-  R.andThen(toJson),
+  R.andThen(toCsv),
+  // R.andThen(toJson),
   R.andThen(console.log),
   R.otherwise(console.error), // catch
 );
@@ -75,14 +79,14 @@ async function fetchInstagramMedia(userId, maxId = null) {
 
 function formatMedia(item) {
   return {
-    caption: item.caption?.text,
-    code: item.code,
-    fbid: item.fbid,
     id: item.id,
-    image_url: item.image_versions2.candidates[0]?.url,
-    media_type: item.media_type,
-    permalink: `http://www.instagram.com/p/${item.code}`,
+    fbid: item.fbid,
     taken_at: item.taken_at,
+    media_type: item.media_type,
+    code: item.code,
+    permalink: `http://www.instagram.com/p/${item.code}`,
+    image_url: item.image_versions2.candidates[0]?.url,
+    caption: item.caption?.text,
   };
 }
 
