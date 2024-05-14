2ba159ccbba89ea3e86b5467efb40b8c61dab224
Paulo Diovani Gonçalves <1295197+paulodiovani@users.noreply.github.com>
2024-05-14 15:32:36 -0300

    Refactor using ramda js

--- scripts/get-instagram-media.js
+++ scripts/get-instagram-media.js
@@ -1,10 +1,11 @@
+const R = require('ramda');
 const axios = require('axios');
 const yargs = require('yargs/yargs');
 
 const ITEM_COUNT = 50; // max == 50
 
 // main function
-async function main() {
+function main() {
   const argv = yargs(process.argv.slice(2))
     .option('username', {
       alias: 'u',
@@ -14,18 +15,22 @@ async function main() {
     .help()
     .parse();
 
-  try {
-    const userInfo = await fetchInstagramUserInfo(argv.username);
-
-    const userId = userInfo.response.body.data.user.id;
-    const mediaItems = await fetchInstagramMedia(parseInt(userId));
-    console.log(JSON.stringify(mediaItems, null, 2));
-  } catch (error) {
-    console.error(JSON.stringify(error, null, 2));
-  }
+  pipeline(argv.username);
 }
 
 // other functions
+
+const toJson = R.curry(JSON.stringify)(R.__, null, 2);
+
+const pipeline = R.pipe(
+  fetchInstagramUserInfo,
+  R.andThen(R.path(['response', 'body', 'data', 'user', 'id'])),
+  R.andThen(parseInt),
+  R.andThen(fetchInstagramMedia),
+  R.andThen(toJson),
+  R.andThen(console.log),
+  R.otherwise(console.error), // catch
+);
 
 function env(key) {
   return process.env[key] || '';
